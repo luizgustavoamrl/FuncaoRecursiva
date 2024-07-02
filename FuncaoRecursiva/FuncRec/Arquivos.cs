ee4cb63b6419d6865112b0c21a87dbb834aceb90
Gustavo-NT <Gustavo-NT@DESKTOP-UGDKJVG>
2024-07-02 14:51:07 -0300

    Add project files.

---
+++ FuncaoRecursiva/FuncRec/Arquivos.cs
@@ -0,0 +1,36 @@
+﻿using FuncaoRecursiva.FuncRec;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncaoRecursiva.FuncRec
+{
+    internal class Arquivos
+    {
+        public static void LerArquivo(int numeroArquivo)
+        {
+            string arquivoCaminho = @"C:\\Arquivos\\Arq" + numeroArquivo + ".txt";
+            if (File.Exists(arquivoCaminho))
+            {
+                Console.WriteLine($"\nTexto do Arquivo {numeroArquivo}:\n");
+                using (StreamReader arquivo = File.OpenText(arquivoCaminho))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+
+                }
+            }
+            string arquivoCaminho2 = @"C:\\Arquivos\\Arq" + (numeroArquivo + 1) + ".txt";
+            if (File.Exists(arquivoCaminho2))
+            {
+                LerArquivo(numeroArquivo + 1);
+            }
+        }
+    }
+}
+
---
+++ FuncaoRecursiva/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using FuncaoRecursiva.FuncRec;
+
+Console.WriteLine("Hello, World!");
+
+Arquivos.LerArquivo(1);
+
+
